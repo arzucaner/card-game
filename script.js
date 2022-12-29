135acec9c0cec4b16a1396abd0183a92cfbb4941
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-29 22:45:59 +0000

    Create card game
    Card game with JavaScript

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard = secondCard + 6 // 23
+
+if(sum < 21) {
+   console.log("Do you want to draw a new card? ")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Cardgames!")
+} else {
+    console.log ("You're out of the game!")
+}
+
+// Check if the person is old enough to enter the nightclub (21)
+ // Log a suitable message to the console in both cases  
+
+let age = 22
+
+ // if less than 21 -> "You can not enter the club!"
+ // else            -> "Welcome!"
+
+ if (age <= 20) {
+    console.log("You can not enter the club!")
+ } else {
+    console.log("Welcome!")
+ }
