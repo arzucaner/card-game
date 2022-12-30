995b84437a29887e14ea725a67736436de461652
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-30 23:26:07 +0000

    Create age practice

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ let age = 22
  } else {
     console.log("Welcome!")
  }
+
+ //Check if the person is elegible for a birthday card from the Lady! (100)
+
+let age = 100
+
+ // if less than 100     -> "Not elegible"
+ // else if exactly 100  -> "Here is your birthday card from the King!"
+ // else                 -> "Not elegible, you have already gotten one"
+
+ if (age < 100) {
+    console.log("Not elegible")
+ } else if (age=== 100) {
+    console.log("Here is your birthday card from the Lady!")
+ } else {
+    console.log("Not elegible, you have already gotton one")
+ }
