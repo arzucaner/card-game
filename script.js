ef9ffd870d0774fd5ea99482a9515e63795df045
Arzu Caner <arzuguneycaner@gmail.com>
2023-01-18 22:42:12 +0000

    Add practice

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-//console.log(4 === 3)  //false
-//console.log(5 > 2)    //true
-//console.log(12 > 12)  //false
-//console.log(3 < 0)    //false
-//console.log(3 >= 3)   //true
-//console.log(11 <= 11) //true
-//console.log(3 <= 2)   //false
+console.log(4 === 3)  //false
+console.log(5 > 2)    //true
+console.log(12 > 12)  //false
+console.log(3 < 0)    //false
+console.log(3 >= 3)   //true
+console.log(11 <= 11) //true
+console.log(3 <= 2)   //false
 
 let firstCard = 10
 let secondCard = 7
+let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard = secondCard
 let hasBlackJack = false
 
+
 //Create a variable called isAlive and assign it to true
 let isAlive = true
-
-// Declare a variable called message and assign it's value to an empty string
 
 //let sumEl = document.getElementById(".sum-el")
 let sumEl = document.querySelector(".sum-el")
@@ -24,12 +24,17 @@ let cardsEl = document.getElementById("cards-el")
 
 //Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
 
 function startGame() {
+   renderGame()
+}
+
+console.log(messageEl)
+
+function randerGame() {
 
    // Render the cards on the page using this format -> "Cards: 10 4"
-   cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+   cardsEl.textContent = "Cards: " + Cards[0] + " " + Cards[1]
    sumEl.textContent = "Sum: " + sum
    if (sum <= 20) {
    message = "Do you want to draw a new card?"
@@ -45,26 +50,32 @@ function startGame() {
    messageEl.textContent = message
 }
 
+function newCard() {
+   let card = 6
+   sum += card
+   renderGame()
+}
 
-//if(sum < 21) {
-  // console.log("Do you want to draw a new card? ")
-//} else if (sum === 21) {
-  //  console.log("Wohoo! You've got Cardgames!")
-  //  hasCard = true
-//} else {
-   // console.log ("You're out of the game!")
-  //  isAlive = false
-//}
+
+if(sum < 21) {
+  console.log("Do you want to draw a new card? ")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Cardgames!")
+    hasCard = true
+} else {
+    console.log ("You're out of the game!")
+    isAlive = false
+}
 
 //Log it out to check that you're doing it right
-//console.log(isAlive)
+console.log(isAlive)
 
 // Check if the person is old enough to enter the nightclub (21)
  // Log a suitable message to the console in both cases  
 
 let age = 22
 
- // if less than 21 -> "You can not enter the club!"
+ //if less than 21 -> "You can not enter the club!"
  // else            -> "Welcome!"
 
  if (age <= 20) {
@@ -75,11 +86,11 @@ let age = 22
 
  //Check if the person is elegible for a birthday card from the Lady! (100)
 
-let age = 100
+//let age = 100
 
- // if less than 100     -> "Not elegible"
- // else if exactly 100  -> "Here is your birthday card from the King!"
- // else                 -> "Not elegible, you have already gotten one"
+ //if less than 100     -> "Not elegible"
+ //else if exactly 100  -> "Here is your birthday card from the King!"
+ //else                 -> "Not elegible, you have already gotten one"
 
  if (age < 100) {
     console.log("Not elegible")
@@ -91,7 +102,7 @@ let age = 100
 
  // Write the conditional according to this rules:
 
- // if less than or equal to 20 -> "Do you want to draw a new card?"
+ //if less than or equal to 20 -> "Do you want to draw a new card?"
  //else if exactly 21 -> "Wohoo! You've got Card!"
  //else -> "You're out of the game!"
 
@@ -117,3 +128,4 @@ let age = 100
    // Call startGame()
    startGame()
  }
+
