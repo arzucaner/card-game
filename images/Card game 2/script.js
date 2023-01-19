877f7bc3dd9e95a09ca224385ae7776850af42cd
Arzu Caner <arzuguneycaner@gmail.com>
2023-01-19 22:42:50 +0000

    Add practice

---
+++ images/Card game 2/script.js
@@ -0,0 +1,16 @@
+// Arrays - ordered lists of items
+
+let featuredPosts = [
+    "Check out my Amazon clone",
+    "Here's the code for my project",
+    "I've just relaunched my portfolio"
+]
+
+console.log( featuredPosts[1])
+// Create an arry that lists your i.e experience, education, licenses, skills or similar
+// The items of the array should be strings
+
+let experience = ["Junior Frontend Developer"]
+
+console.log(experience[1])
+console.log( featuredPosts.length )
